39ba37c86c98b47f8379d5bbd4320bbed68a2024
kawanet <u-suke@kawa.net>
2013-05-29 23:02:38 +0900

    no default storage

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ var Base = require('./core/base');
 var Storage = require('./core/storage');
 var Cursor = require('./core/cursor');
 var Webapi = require('./core/webapi');
-var Memory = require('./storage/memory');
 
 var index = Base.inherit();
 index.use(Storage);
 index.use(Cursor);
 index.use(Webapi);
-// index.use(Memory);
 
 module.exports = index;
