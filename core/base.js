9b7caec203d3347bde9c6fc0355ca17e65f84845
kawanet <u-suke@kawa.net>
2013-05-30 18:04:50 +0900

    use utils.extend()

--- core/base.js
+++ core/base.js
@@ -1,75 +1,93 @@
 /*! core/base.js */
 
+var utils = require('../core/utils');
+
 function inherit(parent) {
-  /** Base class for the inheritance
+
+  /** constructor
    * @class KagoDB
    * @param {Object} options - option parameters
+   * @returns {KagoDB} an instance
+   * @example
+   * var kago = new KagoDB(options);
+   * var kago2 = KagoDB(options); // same as above
    */
 
-  function Base(options) {
-    var key;
-    if (!(this instanceof Base)) return new Base(options);
-    parent.call(this);
-    for (key in Base.options) {
-      this.set(key, Base.options[key]);
-    }
-    for (key in options) {
-      this.set(key, options[key]);
-    }
+  // note that the below doesn't call super classes' constructors.
+
+  function child(options) {
+    if (!(this instanceof child)) return new child(options);
+    this.set(child.options);
+    this.set(options);
   }
 
-  Base.prototype = wrap(parent.prototype);
-  Base.options = copy(parent.options);
+  utils.extend(parent, child);
+
+  child.options = copy(parent.options);
 
   /** load a mixin class which will export instance methods
    * @method KagoDB.use
    * @param {Function} mixin - mixin class which has exporter() class method
    * @returns this class itself for method chaining
+   * @example
+   * var MyKago = KagoDB.inherit();
+   * MyKago.use(SomeMixin);
+   * var kago = new MyKago();
    */
-  Base.use = function(mixin) {
-    mixin.exporter.call(Base.prototype);
-    return Base;
+  child.use = function(mixin) {
+    mixin.exporter.call(child.prototype);
+    return child;
   };
 
-  /** builds an instance as same as new Base()
+  /** builds an instance as same as new child(). It would be good for method chaining.
    * @method KagoDB.build
    * @param {Object} options - option parameters
    * @returns {KagoDB} an instance
+   * @example
+   * var kago = KagoDB.build(options);
    */
-  Base.build = function(options) {
-    return new Base(options);
+  child.build = function(options) {
+    return new child(options);
   };
 
-  /** generates a sub class which inherits this class
+  /** generates a sub class which inherits KagoDB class or its descendant
    * @method KagoDB.inherit
-   * @static
    * @returns a sub class
+   * @example
+   * var MyClass = KagoDB.inherit();
    */
-  Base.inherit = function() {
-    return inherit(Base);
+  child.inherit = function() {
+    return inherit(child);
   };
 
   /** gets a default parameter value for the class
    * @method KagoDB.get
    * @param {String} key - parameter name
    * @returns parameter value
+   * @example
+   * var path = KagoDB.get('path');
    */
-  Base.get = function(key) {
-    return Base.options[key];
+  child.get = function(key) {
+    var opts = child.options || (child.options = {});
+    return opts[key];
   };
 
   /** sets a default parameter value for the class
    * @method KagoDB.set
-   * @param {String} key - parameter name
-   * @param val - new value
+   * @param {String|Object} key - parameter name or a parameters object
+   * @param {any} [val] - new value
    * @returns this class itself for method chaining
+   * @example
+   * KagoDB.set('path', 'data');
+   * KagoDB.set({'storage': 'yaml'});
    */
-  Base.set = function(key, val) {
-    Base.options[key] = val;
-    return Base;
+  child.set = function(key, val) {
+    var opts = child.options || (child.options = {});
+    opts[key] = val;
+    return child;
   };
 
-  return Base;
+  return child;
 }
 
 function copy(parent) {
@@ -80,37 +98,43 @@ function copy(parent) {
   return object;
 }
 
-function wrap(parent) {
-  var anonymous = function() {};
-  anonymous.prototype = parent;
-  return new anonymous();
-}
-
-function Root() {
-  if (!(this instanceof Root)) return new Root(options);
-  this.options = {};
-}
-
-Root.options = {};
+function KagoDB() {}
 
 /** gets a parameter value for the instance parameters
  * @method KagoDB.prototype.get
  * @param {String} key - parameter name
  * @returns parameter value
+ * @example
+ * var kago = new KagoDB({path: 'data'});
+ * var path = kago.get('path');
  */
-Root.prototype.get = function(key) {
-  return this.options[key];
+KagoDB.prototype.get = function(key) {
+  var opts = this.options || (this.options = {});
+  return opts[key];
 };
 
 /** sets a parameter value for the instance parameters
  * @method KagoDB.prototype.set
- * @param {String} key - parameter name
- * @param val - new parameter value
+ * @param {String|Object} keys - parameter name or a parameters object
+ * @param {any} [val] - new parameter value
  * @returns this instance itself for method chaining
+ * @example
+ * var kago = new KagoDB();
+ * kago.set('path', 'data');
+ * kago.set({'storage': 'yaml'});
  */
-Root.prototype.set = function(key, val) {
-  this.options[key] = val;
+KagoDB.prototype.set = function(key, val) {
+  var opts = this.options || (this.options = {});
+  var len = arguments.length;
+  if (len == 2 && key) {
+    opts[key] = val;
+  } else if (len == 1 && key) {
+    var args = key;
+    for (key in args) {
+      opts[key] = args[key];
+    }
+  }
   return this;
 };
 
-module.exports = inherit(Root);
+module.exports = inherit(KagoDB);
