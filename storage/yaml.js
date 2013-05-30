cb700b74d4d6472b2b0c09fd0036b1b933310f6b
kawanet <u-suke@kawa.net>
2013-05-30 09:08:18 +0900

    folder/suffix/decode/encode methods

--- storage/yaml.js
+++ storage/yaml.js
@@ -8,73 +8,96 @@ module.exports = StorageYAML;
 function StorageYAML(options) {
   if (!(this instanceof StorageYAML)) return new StorageYAML(options);
   options = options || {};
-  this.folder(options.path);
+  if (options.path) {
+    this.folder(options.path);
+  }
+  if (options.suffix) {
+    this.suffix(options.suffix);
+  }
 }
 
-StorageYAML.prototype.path = path;
-StorageYAML.prototype.folder = folder;
-StorageYAML.prototype.read = read;
-StorageYAML.prototype.write = write;
-StorageYAML.prototype.remove = remove;
-StorageYAML.prototype.exists = exists;
-StorageYAML.prototype.keys = keys;
+StorageYAML.prototype.folder = function(path) {
+  return this._folder = path || this._folder;
+};
+StorageYAML.prototype.suffix = function(ext) {
+  return this._suffix = ext || this._suffix || '.yaml';
+};
+StorageYAML.prototype.decode = function(source) {
+  return jsyaml.load(source);
+};
+StorageYAML.prototype.encode = function(item) {
+  return jsyaml.dump(item);
+};
 
-function path(id) {
+StorageYAML.prototype.path = function(id) {
   var folder = this.folder();
-  return folder + '/' + id + '.yaml';
-}
+  var suffix = this.suffix();
+  if ('undefined' === typeof folder) {
+    throw new Error('storage folder not defined');
+  }
+  if ('undefined' === typeof suffix) {
+    throw new Error('storage file suffix not defined');
+  }
+  return folder + '/' + id + suffix;
+};
 
-function folder(path) {
-  return this._folder = path || this._folder || 'data';
-}
-
-function read(id, callback) {
+StorageYAML.prototype.read = function(id, callback) {
   callback = callback || NOP;
   var path = this.path(id);
+  var self = this;
   fs.readFile(path, 'utf8', function(err, content) {
     if (err) {
       callback(err);
     } else {
-      var item = jsyaml.load(content);
+      var item = self.decode(content);
       callback(null, item);
     }
   });
-}
+};
 
-function write(id, item, callback) {
+StorageYAML.prototype.write = function(id, item, callback) {
   var path = this.path(id);
-  var yaml = jsyaml.dump(item);
-  fs.writeFile(path, yaml, 'utf8', callback);
-}
+  var encoded = this.encode(item);
+  fs.writeFile(path, encoded, 'utf8', callback);
+};
 
-function remove(id, callback) {
+StorageYAML.prototype.remove = function(id, callback) {
   var path = this.path(id);
   fs.unlink(path, callback);
-}
+};
 
-function exists(id, callback) {
+StorageYAML.prototype.exists = function(id, callback) {
   var file = this.path(id);
   fs.stat(file, function(err, stat) {
     callback(null, !! stat);
   });
-}
+};
 
-function keys(callback) {
+StorageYAML.prototype.keys = function(callback) {
   callback = callback || NOP;
   var folder = this.folder();
+  var suffix = this.suffix();
+  if ('undefined' === typeof folder) {
+    throw new Error('storage folder not defined');
+  }
+  if ('undefined' === typeof suffix) {
+    throw new Error('storage file suffix not defined');
+  }
+  suffix = suffix.toLowerCase();
+  var suflen = suffix.length;
   fs.readdir(folder, function(err, list) {
     if (err) {
       callback(err);
     } else {
       list = list.filter(function(file) {
-        return (file.search(/\.yaml$/i) > -1);
+        return (file.substr(-suflen).toLowerCase() == suffix);
       });
       list = list.map(function(file) {
-        return file.replace(/\.yaml$/i, '');
+        return file.substr(0, file.length - suflen);
       });
       callback(null, list);
     }
   });
-}
+};
 
 function NOP() {}
