702059482d10703987051c21dcc01cbdbcb7000e
kawanet <u-suke@kawa.net>
2013-05-30 23:47:01 +0900

    indent

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,42 +2,42 @@
 
 module.exports = function(grunt) {
 
-    grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jsdoc');
 
-    // Project configuration.
-    grunt.initConfig({
+  // Project configuration.
+  grunt.initConfig({
 
-        // https://github.com/gruntjs/grunt-contrib-jshint
-        jshint: {
-            all: ['Gruntfile.js', '*.js', 'lib/*.js', 'core/*.js', 'test/*.js'],
-            options: {
-                // http://www.jshint.com/docs/
-                // eqnull: true,
-                // proto: true
-            }
-        },
+    // https://github.com/gruntjs/grunt-contrib-jshint
+    jshint: {
+      all: ['Gruntfile.js', '*.js', 'lib/*.js', 'core/*.js', 'test/*.js'],
+      options: {
+        // http://www.jshint.com/docs/
+        // eqnull: true,
+        // proto: true
+      }
+    },
 
-        // https://github.com/pghalliday/grunt-mocha-test
-        mochaTest: {
-            files: ['test/*.test.js'],
-            options: {
-                reporter: 'spec'
-            }
-        },
+    // https://github.com/pghalliday/grunt-mocha-test
+    mochaTest: {
+      files: ['test/*.test.js'],
+      options: {
+        reporter: 'spec'
+      }
+    },
 
-        // https://github.com/krampstudio/grunt-jsdoc-plugin
-        jsdoc: {
-            dist: {
-                src: ['core/*.js', 'storage/*.js'],
-                options: {
-                    destination: 'gh-pages/docs'
-                }
-            }
+    // https://github.com/krampstudio/grunt-jsdoc-plugin
+    jsdoc: {
+      dist: {
+        src: ['core/*.js', 'storage/*.js'],
+        options: {
+          destination: 'gh-pages/docs'
         }
-    });
+      }
+    }
+  });
 
-    // Default task.
-    grunt.registerTask('default', ['jshint', 'mochaTest', 'jsdoc']);
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'mochaTest', 'jsdoc']);
 };
