5707673d8756c234d0af72967d7dca10475e760c
kawanet <u-suke@kawa.net>
2013-06-02 14:41:29 +0900

    tests shares crud_tests.js for basic CRUD tests

--- test/31.json.test.js
+++ test/31.json.test.js
@@ -3,105 +3,44 @@
 var assert = require('chai').assert;
 var fs = require('fs');
 var KagoDB = require('../index');
+var crud_tests = require('./lib/crud_tests');
 
 describe('JSON Storage', function() {
-  var collection;
   var opts = {
     storage: 'json',
     path: './data',
     'json-spaces': ' '
   };
-  var date = (new Date()).toJSON().replace(/\.\d+|\D/g, '');
-  var id1 = 'foo-' + date;
-  var id2 = 'bar-' + date;
-  var item = {
-    string: "FOO",
-    decimal: 123,
-    numeric: 45.67
-  };
-
-  var path1 = opts.path + '/' + id1 + '.' + opts.storage;
-  var path2 = opts.path + '/' + id2 + '.' + opts.storage;
-
-  beforeEach(function() {
-    collection = new KagoDB(opts);
-  });
 
   describe('CRUD', function() {
-    it('write', function(done) {
+    var kit = {};
+    beforeEach(function() {
+      kit.collection = new KagoDB(opts);
+    });
+    crud_tests(kit);
+  });
+
+  describe('File existance', function() {
+    var date = (new Date()).toJSON().replace(/\.\d+|\D/g, '');
+    var id1 = 'foo-' + date;
+    var path1 = opts.path + '/' + id1 + '.' + opts.storage;
+    var item = {
+      string: "FOO",
+      decimal: 123,
+      numeric: 45.67
+    };
+    var collection = new KagoDB(opts);
+
+    it(path1, function(done) {
       collection.write(id1, item, function(err) {
         assert(!err, 'write failed');
-        done();
-      });
-    });
-
-    it('exist', function(done) {
-      collection.exist(id1, function(err, res) {
-        assert(!err, 'exist failed');
-        assert.ok(res, 'exist foo');
-        collection.exist(id2, function(err, res) {
-          assert(!err, 'not-exist failed');
-          assert.ok(!res, 'not-exist bar');
-
-          fs.stat(path1, function(err, stat) {
-            assert(!err, 'file 1 exist');
-            fs.stat(path2, function(err, stat) {
-              assert(err, 'file 2 not exist');
-              done();
-            });
-          });
-        });
-      });
-    });
-
-    it('read', function(done) {
-      collection.read(id1, function(err, res) {
-        assert(!err, 'read failed');
-        assert.isString(res.string, 'read string type');
-        assert.isNumber(res.decimal, 'read decimal type');
-        assert.isNumber(res.numeric, 'read numeric type');
-        assert.equal(res.string, item.string, 'read string content');
-        assert.equal(res.decimal, item.decimal, 'read decimal content');
-        assert.equal(res.numeric, item.numeric, 'read numeric content');
-        collection.read(id2, function(err, res) {
-          assert.ok(err, 'read error detected');
-          done();
-        });
-      });
-    });
-
-    it('index & find', function(done) {
-      collection.index(function(err, list) {
-        assert(!err, 'index failed');
-        assert.ok(list, 'index response');
-        list = list || [];
-        assert.ok(list.length, 'index length');
-
-        collection.find().toArray(function(err, res) {
-          assert(!err, 'find failed');
-          assert.ok(res, 'find response');
-          res = res || [];
-          assert.equal(res.length, list.length, 'find length');
-          done();
-        });
-      });
-    });
-
-    it('erase', function(done) {
-      collection.erase(id1, function(err) {
-        assert(!err, 'erase failed');
-        collection.exist(id1, function(err, res) {
-          assert(!err, 'exist failed');
-          assert.ok(!res, 'not-exist foo');
-          collection.erase(id2, function(err) {
-            assert.ok(err, 'erase error detected');
-
+        fs.stat(path1, function(err, stat) {
+          assert(!err, 'file should exist');
+          collection.erase(id1, function(err) {
+            assert(!err, 'erase failed');
             fs.stat(path1, function(err, stat) {
-              assert(err, 'file 1 exist');
-              fs.stat(path2, function(err, stat) {
-                assert(err, 'file 2 not exist');
-                done();
-              });
+              assert(err, 'file shoud not exist');
+              done();
             });
           });
         });
--- test/42.http_jquery.test.js
+++ test/42.http_jquery.test.js
@@ -3,6 +3,7 @@
 var assert = require('chai').assert;
 var KagoDB = require('../index');
 var jQuery = require('jquery');
+var crud_tests = require('./lib/crud_tests');
 
 describe('Proxy jQuery', function() {
   var endpoint = process.env.TEST_ENDPOINT;
@@ -16,89 +17,17 @@ describe('Proxy jQuery', function() {
 
   if (!endpoint) return;
 
-  var collection;
   var opts = {
     storage: 'http_jquery',
     endpoint: endpoint,
     jquery: jQuery
   };
-  var date = (new Date()).toJSON().replace(/\.\d+|\D/g, '');
-  var id1 = 'foo-' + date;
-  var id2 = 'bar-' + date;
-  var item = {
-    string: "FOO",
-    decimal: 123,
-    numeric: 45.67
-  };
-
-  beforeEach(function() {
-    collection = new KagoDB(opts);
-  });
 
   describe('CRUD', function() {
-    it('write', function(done) {
-      collection.write(id1, item, function(err) {
-        assert(!err, 'write failed ' + err);
-        done();
-      });
+    var kit = {};
+    beforeEach(function() {
+      kit.collection = new KagoDB(opts);
     });
-
-    it('exist', function(done) {
-      collection.exist(id1, function(err, res) {
-        assert(!err, 'exist failed' + err);
-        assert.ok(res, 'exist foo');
-        collection.exist(id2, function(err, res) {
-          assert(!err, 'not-exist failed' + err);
-          assert.ok(!res, 'not-exist bar');
-          done();
-        });
-      });
-    });
-
-    it('read', function(done) {
-      collection.read(id1, function(err, res) {
-        assert(!err, 'read failed' + err);
-        assert.isString(res.string, 'read string type');
-        assert.isNumber(res.decimal, 'read decimal type');
-        assert.isNumber(res.numeric, 'read numeric type');
-        assert.equal(res.string, item.string, 'read string content');
-        assert.equal(res.decimal, item.decimal, 'read decimal content');
-        assert.equal(res.numeric, item.numeric, 'read numeric content');
-        collection.read(id2, function(err, res) {
-          assert.ok(err, 'read error detected');
-          done();
-        });
-      });
-    });
-
-    it('index & find', function(done) {
-      collection.index(function(err, list) {
-        assert(!err, 'index failed' + err);
-        assert.ok(list, 'index response');
-        list = list || [];
-        assert.ok(list.length, 'index length');
-        collection.find().toArray(function(err, res) {
-          assert(!err, 'find failed' + err);
-          assert.ok(res, 'find response');
-          res = res || [];
-          assert.equal(res.length, list.length, 'find length');
-          done();
-        });
-      });
-    });
-
-    it('erase', function(done) {
-      collection.erase(id1, function(err) {
-        assert(!err, 'erase failed' + err);
-        collection.exist(id1, function(err, res) {
-          assert(!err, 'exist failed' + err);
-          assert.ok(!res, 'not-exist foo');
-          collection.erase(id2, function(err) {
-            assert.ok(err, 'erase error detected');
-            done();
-          });
-        });
-      });
-    });
+    crud_tests(kit);
   });
 });
