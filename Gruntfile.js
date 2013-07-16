31e06642091a732ed63446bcce163c752b10342d
kawanet <u-suke@kawa.net>
2013-07-16 19:04:26 +0900

    grunt-quote-json

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-quote-json');
 
   grunt.loadTasks('./tasks');
 
